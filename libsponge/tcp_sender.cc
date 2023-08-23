1b9427ce594805ab3ae53604312317c4d746bf3e
galacy <galacy0420@gmail.com>
2023-08-23 21:22:19 +0800

    finish lab4

--- libsponge/tcp_sender.cc
+++ libsponge/tcp_sender.cc
@@ -10,7 +10,7 @@
 // automated checks run by `make check_lab3`.
 
 template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
+void DUMMY_CODE(Targs &&.../* unused */) {}
 
 using namespace std;
 
@@ -22,17 +22,106 @@ TCPSender::TCPSender(const size_t capacity, const uint16_t retx_timeout, const s
     , _initial_retransmission_timeout{retx_timeout}
     , _stream(capacity) {}
 
-uint64_t TCPSender::bytes_in_flight() const { return {}; }
+uint64_t TCPSender::bytes_in_flight() const { return _outgoing_bytes; }
 
-void TCPSender::fill_window() {}
+void TCPSender::fill_window() {
+
+    size_t curr_window_size = _last_window_size ? _last_window_size : 1;
+
+    while (curr_window_size > _outgoing_bytes) {
+
+        TCPSegment segment;
+        if (!_set_syn_flag) {
+            segment.header().syn = true;
+            _set_syn_flag = true;
+        }
+        segment.header().seqno = next_seqno();
+
+        const size_t payload_size =
+            min(TCPConfig::MAX_PAYLOAD_SIZE, curr_window_size - _outgoing_bytes - segment.header().syn);
+        string payload = _stream.read(payload_size);
+
+
+
+
+        if (!_set_fin_flag && _stream.eof() && payload.size() + _outgoing_bytes < curr_window_size)
+            _set_fin_flag = segment.header().fin = true;
+
+        segment.payload() = Buffer(move(payload));
+
+
+        if (segment.length_in_sequence_space() == 0)
+            break;
+
+
+        if (_outgoing_map.empty()) {
+            _timeout = _initial_retransmission_timeout;
+            _timecount = 0;
+        }
+
+
+        _segments_out.push(segment);
+
+
+        _outgoing_bytes += segment.length_in_sequence_space();
+        _outgoing_map.insert(make_pair(_next_seqno, segment));
+
+        _next_seqno += segment.length_in_sequence_space();
+
+
+        if (segment.header().fin)
+            break;
+    }
+}
 
 //! \param ackno The remote receiver's ackno (acknowledgment number)
 //! \param window_size The remote receiver's advertised window size
-void TCPSender::ack_received(const WrappingInt32 ackno, const uint16_t window_size) { DUMMY_CODE(ackno, window_size); }
+void TCPSender::ack_received(const WrappingInt32 ackno, const uint16_t window_size) {
+    size_t abs_seqno = unwrap(ackno, _isn, _next_seqno);
+    if (abs_seqno > _next_seqno)
+        return;
+
+    for (auto iter = _outgoing_map.begin(); iter != _outgoing_map.end();) {
+
+        const TCPSegment &seg = iter->second;
+        if (iter->first + seg.length_in_sequence_space() <= abs_seqno) {
+            _outgoing_bytes -= seg.length_in_sequence_space();
+            iter = _outgoing_map.erase(iter);
+
+            _timeout = _initial_retransmission_timeout;
+            _timecount = 0;
+        }
+
+        else
+            break;
+    }
+    _consecutive_retransmissions_count = 0;
+
+    _last_window_size = window_size;
+    fill_window();
+}
 
 //! \param[in] ms_since_last_tick the number of milliseconds since the last call to this method
-void TCPSender::tick(const size_t ms_since_last_tick) { DUMMY_CODE(ms_since_last_tick); }
+void TCPSender::tick(const size_t ms_since_last_tick) {
+    _timecount += ms_since_last_tick;
 
-unsigned int TCPSender::consecutive_retransmissions() const { return {}; }
+    auto iter = _outgoing_map.begin();
 
-void TCPSender::send_empty_segment() {}
+    if (iter != _outgoing_map.end() && _timecount >= _timeout) {
+
+        if (_last_window_size > 0)
+            _timeout *= 2;
+        _timecount = 0;
+        _segments_out.push(iter->second);
+        
+        ++_consecutive_retransmissions_count;
+    }
+}
+
+unsigned int TCPSender::consecutive_retransmissions() const { return _consecutive_retransmissions_count; }
+
+void TCPSender::send_empty_segment() {
+    TCPSegment segment;
+    segment.header().seqno = next_seqno();
+    _segments_out.push(segment);
+}
